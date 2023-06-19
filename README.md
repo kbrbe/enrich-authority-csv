ddf1d94b6534d232595fe4bff2d94782d4678901
Sven Lieber <Sven.Lieber@ugent.be>
2023-06-19 22:26:27 +0200

    initial commit with adapted code from a feature branch in https://github.com/kbrbe/beltrans-data-integration/

--- README.md
+++ README.md
@@ -1,2 +1,104 @@
-# enrich-authority-csv-via-isni
-A python script that uses the ISNI SRU API to complete a CSV file with missing identifiers based on an available ISNI identifier column
+# Enrich authority csv via ISNI
+
+A python script that uses the ISNI SRU API to complete a CSV file with missing data based on an available ISNI identifier column
+
+ISNI is the [ISO 27729:2012](https://www.iso.org/standard/44292.html) standard name identifier that uniquely identifies public entities who contributed to creative works.
+As an ISNI registration agency, the Royal Library of Belgium (KBR) can request ISNI identifiers from the ISNI central database.
+
+Given a CSV file where each row is a contributor to creative works, this script uses the ISNI identifier in one of the columns to
+fill data gaps in other specified columns based on data available via the ISNI Search/Retrieve via URL (SRU) API.
+
+## Usage
+
+Create and activate a Python virtual environment
+```bash
+
+# Create a new Python virtual environment
+python3 -m venv py-enrich-csv-via-isni-env
+
+# Activate the virtual environment
+source py-request-isni-env/bin/activate
+
+# Install dependencies
+pip -r requirements.txt
+```
+
+Given a CSV file with the input data, you can call the script in the following way to use the ISNI identifier of the column `isniID`
+for requests against the ISNI SRU API to fill possible gaps in the column
+
+* `kbrID` with the `KBR` identifier found in public ISNI records (Royal Library of Belgium)
+* `bnfID` with the `BNF` identifier found in public ISNI records (National Library of France)
+* `ntaID` with the `NTA` identifier found in public ISNI records (Royal Library of the Netherlands)
+* `gender` with gender information found in ISNI records
+* `nationality` with nationality information found in ISNI records
+
+```bash
+python get_identifier_from_isni.py \
+  -i input-file.csv \
+  -o enriched-file.csv \
+  --column-name-isni isniID \
+  --wait 0.5
+  --identifiers kbrID=KBR ntaID=NTA bnfID=BNF gender=gender nationalities=nationality
+```
+
+Please note that you should provide a `.env` file with your credentials for the ISNI SRU API:
+
+```
+ISNI_SRU_USERNAME=yourUser
+ISNI_SRU_PASSWORD=yourPassword
+```
+
+The script will first provide some statistics of how many rows are possible to be enriched
+by looping over the input file in a streaming fashion.
+Afterwards the script starts requesting data, progress is shown in a progress bar.
+
+Example statistics that are printed before the requests
+
+```bash
+In total, the file contains 25633 lines from which 13601 have an ISNI (53.06%)
+Stats for column "kbrIDs" that should be enriched via "KBR" field from the ISNI database
+12186 KBR identifiers are missing and we want to get them (47.54%).
+From those 12186 missing, we could enrich 3016, because they have an ISNI (24.75%)
+
+Stats for column "ntaIDs" that should be enriched via "NTA" field from the ISNI database
+19177 NTA identifiers are missing and we want to get them (74.81%).
+From those 19177 missing, we could enrich 7992, because they have an ISNI (41.67%)
+
+Stats for column "bnfIDs" that should be enriched via "BNF" field from the ISNI database
+19126 BNF identifiers are missing and we want to get them (74.61%).
+From those 19126 missing, we could enrich 7105, because they have an ISNI (37.15%)
+
+Stats for column "gender" that should be enriched via "gender" field from the ISNI database
+14279 gender identifiers are missing and we want to get them (55.71%).
+From those 14279 missing, we could enrich 3373, because they have an ISNI (23.62%)
+
+Stats for column "nationalities" that should be enriched via "nationality" field from the ISNI database
+15505 nationality identifiers are missing and we want to get them (60.49%).
+From those 15505 missing, we could enrich 3751, because they have an ISNI (24.19%)
+
+```
+
+Example output of the progress bar that constantly updates the number of found data elements
+
+```bash
+found KBR 1398,NTA 6504,BNF 1234,gender 159,nationality 137:  50%|█████████████████████████████████████████████████▉                                                 | 12218/24250 [36:57<34:54,  5.74it/s]
+```
+
+## Software tests
+
+Functions in `lib.py` contain doctests. An additional overal test file with integration tests is currently still missing.
+
+## License
+
+The license of this software was chosen using https://ufal.github.io/public-license-selector and based on licenses of software libraries used by this repo:
+
+| Library | Description | License |
+|---------|-------------|---------|
+| certifi | Providing the list of Mozilla's carefully curated collection of Root certificates, such that we can communicate securely with the ISNI server via https. | [MPL 2.0](https://www.mozilla.org/en-US/MPL/2.0/) |
+| charset-normalizer | A library to help reading text from an unknown charset encoding, this library is used by the `requests` library we are using. | [MIT](https://opensource.org/licenses/MIT) |
+| idna | A library to handle internationalized domain names. It is used by `urllib3` and therefore indirectly by our project . | [BSD 3-clause](https://opensource.org/licenses/BSD-3-Clause) |
+| python-dotenv | Functionality to load a .env environment file and make the environment variables accessible in Python. We use this library to provide the functionality of specifying the ISNI API URL in an environment variable instead of via a commandline parameter. | [BSD 3-clause](https://opensource.org/licenses/BSD-3-Clause) |
+| requests | We use this library to perform HTTP requests against the ISNI APIs. | [Apache 2.0](https://www.apache.org/licenses/LICENSE-2.0) |
+| tqdm | A library to provide a user-friendly progress bar, used to show the progress of enriched data from the ISNI database. | [MPL 2.0](https://www.mozilla.org/en-US/MPL/2.0/) |
+| urllib3 | Used by the requests library to make requests, thus implicitly used to make API requests against the ISNI APIs. | [MIT](https://opensource.org/licenses/MIT) |
+
---
+++ enrich_authority_csv_via_isni/lib.py
@@ -0,0 +1,316 @@
+import urllib
+import requests
+import xml.etree.ElementTree as ET
+
+NS_SRW = 'http://www.loc.gov/zing/srw/'
+ALL_NS = {'srw': NS_SRW}
+
+
+# -----------------------------------------------------------------------------
+def getBnFIdentifierWithControlCharacter(identifier):
+  """This function computes the BnF control character based on documentation from BnF.
+
+  It correctly works for the two following real life examples
+  >>> getBnFIdentifierWithControlCharacter('cb13741679')
+  'cb13741679s'
+  >>> getBnFIdentifierWithControlCharacter('cb11896963')
+  'cb11896963c'
+
+  Also return the correct control character if the 'cb' prefix is missing
+  >>> getBnFIdentifierWithControlCharacter('11896963')
+  'cb11896963c'
+
+  Return the same identifier if there is already a valid control character
+  >>> getBnFIdentifierWithControlCharacter('cb11896963c')
+  'cb11896963c'
+
+  Return a corrected identifier if the given control character is wrong
+  >>> getBnFIdentifierWithControlCharacter('cb11896963d')
+  'cb11896963c'
+
+  Throw an error if the identifier is too short
+  >>> getBnFIdentifierWithControlCharacter('cb118969c')
+  Traceback (most recent call last):
+      ...
+  Exception: Invalid BnF identifier, too short: cb118969c
+
+  Throw an error if the identifier is too long
+  >>> getBnFIdentifierWithControlCharacter('cb118969631c')
+  Traceback (most recent call last):
+      ...
+  Exception: Invalid BnF identifier, too long: cb118969631c
+  """
+
+  correspondenceTable = ['0','1','2','3','4','5','6','7','8',
+                         '9','b','c','d','f','g','h','j','k',
+                         'm','n','p','q','r','s','t','v','w',
+                         'x','z']
+
+  # make sure the identifier starts with 'cb'
+  identifier = f'cb{identifier}' if not identifier.startswith('cb') else identifier
+
+  if len(identifier) == 11:
+    # perfect length, so there seems to be already a control character
+    # don't trust it, to be sure compute the control character again
+    return getBnFIdentifierWithControlCharacter(identifier[0:10])
+  elif len(identifier) > 11:
+    # this identifier is too long for being a BnF identifier
+    raise Exception(f'Invalid BnF identifier, too long: {identifier}')
+  elif len(identifier) < 10:
+    # this identifier is too short for being a BnF identifier
+    raise Exception(f'Invalid BnF identifier, too short: {identifier}')
+
+  values = []
+  position = 1
+  for digit in identifier:
+    digitBase10Value = correspondenceTable.index(digit)
+    values.append(digitBase10Value * position)
+    position += 1
+  sumMod29 = sum(values)%29
+  return identifier + str(correspondenceTable[sumMod29])
+
+# -----------------------------------------------------------------------------
+def checkIfColumnsExist(inputColumnNames, outputColumnNames):
+    """This function checks if all names of the second list are present in the first, if not an Error is raised.
+
+    The function simply returns true if all names are present
+    >>> checkIfColumnsExist(['a', 'b', 'c'], ['a', 'c'])
+    True
+
+    If a name is missing an Exception is thrown mentioning which names are missing
+    >>> checkIfColumnsExist(['a', 'b', 'c'], ['a', 'd'])
+    Traceback (most recent call last):
+        ...
+    Exception: The following requested column is not in the input: {'d'}
+    """
+    inputColumns = set(inputColumnNames)
+    outputColumns = set(outputColumnNames)
+    nonExistentColumns = outputColumns.difference(inputColumns)
+    if len(nonExistentColumns) > 0:
+      text = 'columns are' if len(nonExistentColumns) > 1 else 'column is'
+      raise Exception(f'The following requested {text} not in the input: {nonExistentColumns}')
+    else:
+        return True
+
+# -----------------------------------------------------------------------------
+def getElementValue(elem, sep=';'):
+  """This function returns the value of the element if it is not None, otherwise an empty string.
+
+  The function returns the 'text' value if there is one
+  >>> class Test: text = 'hello'
+  >>> obj = Test()
+  >>> getElementValue(obj)
+  'hello'
+
+  It returns nothing if there is no text value
+  >>> class Test: pass
+  >>> obj = Test()
+  >>> getElementValue(obj)
+  ''
+
+  And the function returns a semicolon separated list in case the argument is a list of objects with a 'text' attribute
+  >>> class Test: text = 'hello'
+  >>> obj1 = Test()
+  >>> obj2 = Test()
+  >>> getElementValue([obj1,obj2])
+  'hello;hello'
+  """
+  if elem is not None:
+    if isinstance(elem, list):
+      valueList = list()
+      for e in elem:
+        if hasattr(e, 'text'):
+          valueList.append(e.text)
+      return ';'.join(valueList)
+    else:
+      if hasattr(elem, 'text'):
+        return elem.text
+  
+  return ''
+
+# -----------------------------------------------------------------------------
+def atLeastOneIdentifierMissing(row, columnNames):
+  """This function returns true if at least one of the given columns are empty.
+  >>> atLeastOneIdentifierMissing({'kbrIDs': '', 'ntaIDs':''}, ['kbrIDs', 'ntaIDs'])
+  True
+  >>> atLeastOneIdentifierMissing({'kbrIDs': '123', 'ntaIDs':''}, ['kbrIDs', 'ntaIDs'])
+  True
+  >>> atLeastOneIdentifierMissing({'kbrIDs': '123', 'ntaIDs':'456'}, ['kbrIDs', 'ntaIDs'])
+  False
+  """
+  
+  relevantValues = [row[c] for c in columnNames]
+  return True if any([True for v in relevantValues if v == '' ]) else False
+
+
+# -----------------------------------------------------------------------------
+def countISNIs(isniString, delimiter=';', validateISNI=False):
+  """Counts how many ISNIs are in the delimited string.
+
+  An empty string should be counted as 0 ISNIs
+  >>> countISNIs('')
+  0
+
+  One ISNI should be counted
+  >>> countISNIs('0000000000000001')
+  1
+  
+  Multiple ISNI values should be counted
+  >>> countISNIs('0000000000000001;0000000000000002')
+  2
+
+  Multiple value should be counted, in default even if not valid ISNIs
+  >>> countISNIs('001;002')
+  2
+
+  If something does not seem like an ISNI (currently only checking for length) an exception is thrown
+  >>> countISNIs('0001', validateISNI=True)
+  Traceback (most recent call last):
+   ...
+  Exception: Invalid ISNI: "0001"
+
+  >>> countISNIs('000000000001;002', validateISNI=True)
+  Traceback (most recent call last):
+   ...
+  Exception: Invalid ISNI: "000000000001"
+  
+  """
+  if isniString == '':
+    return 0
+  isniList = isniString.split(delimiter)
+  if validateISNI:
+    for isni in isniList:
+      if len(isni) != 16:
+        raise Exception(f'Invalid ISNI: "{isni}"')
+  else:
+    return len(isniList)
+
+
+
+# -----------------------------------------------------------------------------
+def initializeCounters(countReader, identifiers, isniColumnName, nationalityColumnName=None):
+  """This function counts statistics from the given arra of dicts (or DictReader).
+
+  >>> rows = [{'kbrIDs':'', 'isniIDs':'001','ntaIDs':''},
+  ... {'kbrIDs':'123', 'isniIDs':'', 'ntaIDs':''},
+  ... {'kbrIDs':'', 'ntaIDs':'', 'isniIDs':''},
+  ... {'kbrIDs':'','ntaIDs':'','isniIDs':'002;003'},
+  ... {'kbrIDs':'123','ntaIDs':'456','isniIDs':'002;003'}]
+  >>> initializeCounters(rows, {'kbrIDs':'KBR', 'ntaIDs':'NTA'}, 'isniIDs')
+  {'numberRows': 5, 'numberRowsHaveISNI': 3, 'numberISNIs': 5, 'numberRowsMissingAtLeastOneIdentifier': 4, 'KBR': {'numberMissingIdentifierRows': 3, 'numberISNIs': 5, 'numberRowsToBeEnrichedHaveISNI': 2, 'numberRowsThatCannotBeEnriched': 1}, 'NTA': {'numberMissingIdentifierRows': 4, 'numberISNIs': 5, 'numberRowsToBeEnrichedHaveISNI': 2, 'numberRowsThatCannotBeEnriched': 2}}
+  """
+
+  # initialize counters
+  counters = {'numberRows': 0, 'numberRowsHaveISNI': 0, 'numberISNIs': 0, 'numberRowsMissingAtLeastOneIdentifier': 0}
+  for column, isniSourceName in identifiers.items():
+    counters[isniSourceName] = {
+      'numberMissingIdentifierRows': 0,
+      'numberISNIs': 0,
+      'numberRowsToBeEnrichedHaveISNI': 0,
+      'numberRowsThatCannotBeEnriched': 0,
+      'numberFoundISNIRows': 0,
+      'numberFoundISNIs': 0
+    }
+  
+
+  identifierColumns = identifiers.keys()
+  for row in countReader:
+
+    # do some general counting for the row
+    counters['numberRows'] += 1
+    counters['numberISNIs'] += countISNIs(row[isniColumnName])
+    if atLeastOneIdentifierMissing(row, identifierColumns):
+      counters['numberRowsMissingAtLeastOneIdentifier'] += 1
+
+    if row[isniColumnName] != '':
+        counters['numberRowsHaveISNI'] += 1
+
+    # count for the specific identifiers we want to add via ISNI
+    for columnName, isniSourceName in identifiers.items():
+
+      counters[isniSourceName]['numberISNIs'] += countISNIs(row[isniColumnName])
+      # the identifier column is empty, a possible candidate to be enriched
+      if row[columnName] == '':
+        counters[isniSourceName]['numberMissingIdentifierRows'] += 1
+
+        # If there is also an ISNI there is the chance that we can enrich it
+        if row[isniColumnName] == '':
+          counters[isniSourceName]['numberRowsThatCannotBeEnriched'] += 1
+        else:
+          counters[isniSourceName]['numberRowsToBeEnrichedHaveISNI'] += 1
+
+  return counters
+
+# -----------------------------------------------------------------------------
+def getPrefixedIdentifier(identifier, identifierName):
+  if identifierName == 'NTA':
+    return f'p{identifier}'
+  elif identifierName == 'BNF':
+    return getBnFIdentifierWithControlCharacter(identifier)
+  else:
+    return identifier
+
+
+
+
+# -----------------------------------------------------------------------------
+def extractIdentifier(xmlContent, identifierName, delimiter=';'):
+  """This function tries to extract the identifier with the given name. If not found it returns None."""
+
+  root = ET.fromstring(xmlContent)
+
+  if identifierName == 'nationality':
+    nationalities = set()
+    for record in root.findall('srw:records/srw:record/srw:recordData/responseRecord/ISNIAssigned/ISNIMetadata/identity/personOrFiction/additionalInformation/nationality', ALL_NS):
+      nationalities.add(record.text)
+    return delimiter.join(sorted(nationalities))
+
+  if identifierName == 'gender':
+    genderInfo = set()
+    for record in root.findall('srw:records/srw:record/srw:recordData/responseRecord/ISNIAssigned/ISNIMetadata/identity/personOrFiction/additionalInformation/gender', ALL_NS):
+      genderInfo.add(record.text)
+    return delimiter.join(sorted(genderInfo))
+
+  for record in root.findall('srw:records/srw:record/srw:recordData/responseRecord/', ALL_NS):
+    sourceTags = record.findall('./ISNIMetadata/sources')
+
+    if sourceTags:
+      for source in sourceTags:
+ 
+        sourceName = getElementValue(source.find('codeOfSource'))
+        identifier = getElementValue(source.find('sourceIdentifier'))
+
+        if sourceName == identifierName:
+          return identifier
+
+    # if this statement is reached nothing was found so we return None
+    return None
+
+# -----------------------------------------------------------------------------
+def requestRecord(url, payload):
+
+  try: 
+    payloadStr = urllib.parse.urlencode(payload, safe=',+*\\')
+    r = requests.get(url, params=payloadStr)
+    r.raise_for_status()
+
+    return r.content
+
+  except requests.exceptions.Timeout:
+    print(f'There was a timeout in iteration {counter}')
+  except requests.exceptions.TooManyRedirects:
+    print(f'There were too many redirects in iteration {counter}')
+  except requests.exceptions.HTTPError as err:
+    print(f'There was an HTTP response code which is not 200 in iteration {counter}')
+  except requests.exceptions.RequestException as e:
+    print(f'There was an exception in the request')
+  except Exception as e:
+    print(f'There was a general exception!')
+    print(e)
+
+
+
+# -----------------------------------------------------------------------------
+if __name__ == "__main__":
+  import doctest
+  doctest.testmod()
